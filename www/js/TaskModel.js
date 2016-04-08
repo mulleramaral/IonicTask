7eaf5b99ceaeb554b6e8221b40ddfd6066ab0114
Miiller caetano do amaral <miilleramaral@gmail.com>
2016-04-07 22:16:00 -0300

    adicionando e removendo

--- www/js/TaskModel.js
+++ www/js/TaskModel.js
@@ -1,6 +1,20 @@
 function getTasks(){
+  //Lista de Itens
   this.items = [
     {nome:'Item 01',finalizada:false},
     {nome:'Item 02',finalizada:false},
     {nome:'Item 01',finalizada:false}];
+
+    //Adicionar Item
+    this.add = function(item)
+    {
+      this.items.push(item);
+    }
+
+    //Remover Item
+    this.remove = function(item)
+    {
+      var pos = this.items.indexOf(item);
+      this.items.splice(pos,1);
+    }
 }
--- www/js/app.js
+++ www/js/app.js
@@ -12,21 +12,56 @@ app.run(function($ionicPlatform) {
       StatusBar.styleDefault();
     }
   });
-})
+});
 
-app.controller('mainController',function($scope){
+app.controller('mainController',function($scope,$ionicPopup,$timeout){
    var tasks = new getTasks();
-   $scope.lista = tasks.items;
 
+   $scope.lista            = tasks.items;
    $scope.exibeFinalizadas = false;
+   $scope.removeStatus     = false;
 
    // Marca True ou False
    $scope.onMarkTask = function(item){
-     console.log("onMarkTask" + item.nome);
      item.finalizada = !item.finalizada;
-   }
+   };
 
+   //Esconder finalizadas ou exibir
    $scope.esconderFinalizada = function(item){
       return item.finalizada && !$scope.exibeFinalizadas;
+   }
+
+   function getItem(item)
+   {
+     $scope.data = {};
+     $scope.data.newTask = "";
+
+     $ionicPopup.show({
+       title: "Nova Tarefa",
+       scope: $scope,
+       template: "<input type='text' placeholder='Tarefa' autofocus='True' ng-model='data.newTask'>",
+       buttons: [
+         {text: 'Ok',
+         onTap: function(e){
+           item.nome = $scope.data.newTask;
+           tasks.add(item);
+         }},
+         {text: 'Cancelar'}
+       ]
+     });
+   }
+
+   $scope.addItem = function()
+   {
+     var item = {nome:'',finalizada:false};
+     getItem(item);
+   }
+
+   $scope.deleteItem = function(item){
+     tasks.remove(item);
+   }
+
+   $scope.onClickRemove = function(){
+     $scope.removeStatus = !$scope.removeStatus
    }
 });
